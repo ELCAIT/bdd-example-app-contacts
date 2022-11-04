d6e28216951825b87de84e2890c191a2b16f5a01
Mark Spörndli <mark.spoerndli@elca.ch>
2022-11-04 19:15:19 +0100

    Implement cucumber test in cypress

--- contacts-ui-tests/cypress/support/commands.js
+++ contacts-ui-tests/cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('createContacts', (body) => {
+    cy.fixture('contacts.json').then((contacts) => {
+        contacts.forEach((contact) => {
+            cy.request({
+                method: 'POST',
+                url: '/contacts',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                failOnStatusCode: false,
+                body: {
+                    name: contact.name,
+                    ...body
+                }
+            }).then((response) => {
+                expect(response.status).to.equal(201);
+            });
+        });
+    });
+});
